b1593f9fb097b55ffa65df56cc686d74f5ef793a
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-01-27 14:47:02 +0330

    Remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,4 @@ if (process.env.NODE_ENV === "development") {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
