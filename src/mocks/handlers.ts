f7381f8b5579da8dfb5b005d90652aa60021d8f2
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-01-27 13:26:19 +0330

    Complete the handler get method

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,11 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("/hello", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        response: "Hello World!",
+      })
+    );
+  }),
+];
