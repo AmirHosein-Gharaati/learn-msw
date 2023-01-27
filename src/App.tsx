ebef22b0500a9239aaa0c629decdaba0830431cc
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-01-27 14:55:21 +0330

    Use axios for api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [helloStatement, setHelloStatement] = useState("Hello World!");
+
+  useEffect(() => {
+    axios
+      .get("/hello")
+      .then((res) => res.data)
+      .then((data) => setHelloStatement(data.response));
+  }, []);
+
   return (
     <div>
-      <p>Hello World!</p>
+      <p>{helloStatement}</p>
     </div>
   );
 }
