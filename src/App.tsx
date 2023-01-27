7592d7bdde62c0bd72070db63628bc70a4aee537
Amirhosein Gharaati <amirgh1380@gmail.com>
2023-01-27 18:30:44 +0330

    Update App.tsx for calling mock API

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,58 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useState } from "react";
+import { LoginRequest, LoginResponse } from "./types/user.type";
 
 function App() {
-  const [helloStatement, setHelloStatement] = useState("Hello World!");
+  const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorResponse, setErrorResponse] = useState("");
 
-  useEffect(() => {
+  function sendLoginRequest() {
+    const loginRequest: LoginRequest = {
+      username: username,
+      password: password,
+    };
+
     axios
-      .get("/hello")
-      .then((res) => res.data)
-      .then((data) => setHelloStatement(data.response));
-  }, []);
+      .post<LoginRequest, AxiosResponse<LoginResponse>>("/login", loginRequest)
+      .then((res) => {
+        setErrorResponse("");
+
+        const { data } = res;
+
+        setAccessToken(data.access);
+        setRefreshToken(data.refresh);
+      })
+      .catch((err: AxiosError) => {
+        const errorMessage = err.response?.statusText as string;
+
+        setErrorResponse(errorMessage);
+
+        setAccessToken("");
+        setRefreshToken("");
+      });
+  }
 
   return (
     <div>
-      <p>{helloStatement}</p>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit" onClick={() => sendLoginRequest()}>
+        Send Request
+      </button>
+      <p style={{ color: "red" }}>{errorResponse}</p>
+      <p>Access token: {accessToken}</p>
+      <p>Refresh token: {refreshToken}</p>
     </div>
   );
 }
